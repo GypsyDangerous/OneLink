b9d23b4f5f1ee9a89ab812ae48a9e5e69bb41af3
David Snyder <davidgraygs4@gmail.com>
2020-12-03 22:24:15 -0500

    refactor the apollo client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,25 +2,28 @@ import GlobalStyle from "../components/shared/GlobalStyle";
 import SEO from "../components/shared/SEO";
 import { UserContextProvider } from "../contexts/userContext";
 import { ApolloProvider } from "@apollo/client";
-import client from "../graphql/client"
+import client from "../graphql/client";
 
 function App({ children }) {
 	return (
 		<>
 			<SEO title="OneLink | Get Started" />
 			<GlobalStyle />
-			<ApolloProvider client={client}>
-				<UserContextProvider>{children}</UserContextProvider>
-			</ApolloProvider>
+
+			{children}
 		</>
 	);
 }
 
 function MyApp({ Component, pageProps }) {
 	return (
-		<App>
-			<Component {...pageProps} />;
-		</App>
+		<ApolloProvider client={client}>
+			<UserContextProvider>
+				<App>
+					<Component {...pageProps} />;
+				</App>
+			</UserContextProvider>
+		</ApolloProvider>
 	);
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,30 @@
 import useUser from "../hooks/useUser";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import loginMutation from "../graphql/loginMutation";
 import client from "../graphql/client";
+import { setAccessToken } from "../auth/accessToken";
 
 export default function Home() {
-	const [data, setData] = useState({});
+	const [loading, setLoading] = useState<boolean>(true);
 
-	const { user } = useUser({ redirectTo: "/landing", as: "/" });
+	useEffect(() => {
+		fetch(`${process.env.NEXT_PUBLIC_API_URL}/refresh_token`, {
+			method: "POST",
+			credentials: "include",
+		}).then(async x => {
+			const { accessToken } = await x.json();
+			setAccessToken(accessToken);
+			setLoading(false);
+		});
+	}, []);
+
+	if (loading) {
+		return <h1>loading...</h1>;
+	}
 
 	return (
 		<div>
-			<button
-				onClick={() =>
-					setData(
-						client
-							.mutate({
-								mutation: loginMutation,
-								variables: {
-									email: "davidgraygs4@gmail.com",
-									password: "Junglecraft6",
-								},
-							})
-							.then(data => data)
-							.catch(console.log)
-					)
-				}
-			>
-				login
-			</button>
+			<h1>Hello next</h1>
 		</div>
 	);
 }
