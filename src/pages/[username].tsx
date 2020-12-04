8ac8839b8187ac9a5b2c192106e52ffe0f9b7494
David Snyder <davidgraygs4@gmail.com>
2020-12-04 18:21:06 -0500

    start adding functionality to dynamic page

--- src/pages/[username].tsx
+++ src/pages/[username].tsx
@@ -1,3 +1,37 @@
-export default function Page() {
+import client from "../graphql/client";
+import { gql } from "@apollo/client";
+
+export default function Page(props) {
+	console.log(props)
 	return <div></div>;
+}
+
+const pageQuery = gql`
+	query getPage($name: String!) {
+		page(name: $name) {
+			links {
+				path,
+				image,
+				name,
+				order,
+				color,
+				active,
+				image
+			}
+			theme
+		}
+	}
+`;
+
+export async function getServerSideProps(ctx) {
+	const { username } = ctx.query;
+	try {
+		const response = await client.query({ query: pageQuery, variables: {name: username} });
+		if(!response.data.page) throw new Error("invalid name")
+		const {data} = response
+		return {props: data}
+	} catch (err) {
+		console.log(err.message);
+		return { notFound: true};
+	}
 }
