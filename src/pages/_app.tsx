5c1ca7388ae22d2d9c8b69cc43049ff25fc31a48
David Snyder <davidgraygs4@gmail.com>
2020-12-04 23:18:42 -0500

    add in useUser to global wrapper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,26 @@ import SEO from "../components/shared/SEO";
 import { UserContextProvider } from "../contexts/userContext";
 import { ApolloProvider } from "@apollo/client";
 import client from "../graphql/client";
+import useUser from "../hooks/useUser";
+
+// import FingerprintJS from '@fingerprintjs/fingerprintjs';
+
+// (async () => {
+//   // We recommend to call `load` at application startup.
+//   const fp = await FingerprintJS.load();
+
+//   // The FingerprintJS agent is ready.
+//   // Get a visitor identifier when you'd like to.
+//   const result = await fp.get();
+
+//   // This is the visitor identifier:
+//   const visitorId = result.visitorId;
+//   console.log(visitorId);
+// })();
 
 function App({ children }) {
+	const { loading, ...userData } = useUser();
+
 	return (
 		<>
 			<SEO title="OneLink | Get Started" />
