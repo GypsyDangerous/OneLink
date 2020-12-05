66fead395489272e9bfad2ab1ecbc367dd49fe2e
David Snyder <davidgraygs4@gmail.com>
2020-12-05 09:15:05 -0500

    add custom error page

--- src/components/shared/GlobalStyle.tsx
+++ src/components/shared/GlobalStyle.tsx
@@ -10,7 +10,7 @@ const GlobalStyle = createGlobalStyle`
     	padding: 0;
         margin: 0;
 		font-family: 'Poppins', sans-serif;
-        overflow: hidden;
+        /* overflow: hidden */
         box-sizing: border-box !important;
     }
 
@@ -27,7 +27,93 @@ const GlobalStyle = createGlobalStyle`
 	::-webkit-scrollbar {
 		width: 8px;
 	  }
+
+	  .grid{
+	padding: 40px 77px;
+	display: grid;
+	width: 100vw;
+	height: 100vh;
+	grid-template-areas: 
+	"header header"
+	"img text"
+	"footer footer";
+}
+
+
+
+.header{
+	grid-area: header;
+	/* padding: 1.5rem */
+	text-transform: uppercase;
+	font-weight: bold;
+	font-size: 1.5rem;
+	/* font-family: Inconsolata, "sans-serif" */
+	margin-bottom: 2rem;
+}
+
+.grid img{
+	grid-area: img;
+	width: 80%;
+	max-width: 540px;
+	min-width: 200px;
+}
+
+.text{
+
+}
+
+.text h1{
+	font-size: 4rem;
+	font-weight: bold;
+	max-width: 590px;
+}
+
+.text p{
+	margin-top: 2.5rem;
+	max-width: 390px;
+	font-size: 1.5rem;
+}
+
+.text a{
+	color: white;
+	text-transform: uppercase;
+	text-decoration: none;
+	display: inline-block;
+	margin-top: 4rem;
+	font-size: .75rem;
+	padding: 1.5rem 2rem ;
+	background: #333333;
+}
+
+.footer{
+	grid-area: footer;
+	align-self: end;
+	justify-self: center;
+	margin-top: auto;
+	font-size: .75rem;
+}
+
+@media screen and (max-width: 900px){
+	.grid{
+		padding: 30px 67px;
+		grid-template-areas:
+		"header"
+		"img"
+		"text"
+		"footer";
+		justify-content: center;
+		align-items: center;
+	}
+
+	.text h1{
+		font-size: 2rem;
+	}
+
+	.text p{
+		font-size: 1.25rem;
+	}
+}
 	  
 `;
 
-export default GlobalStyle
+export default GlobalStyle;
---
+++ src/pages/404.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link"
+
+function CustomError() {
+	return (
+		<div className="grid">
+			<div className="header">
+				<h1>404 Not Found</h1>
+			</div>
+			<img src="/Scarecrow.png" alt="" />
+			<div className="text">
+				<h1>I have bad news for you</h1>
+				<p>The page you are looking for might be removed or is temporarily unavailable</p>
+				<a href="#">Back to homepage</a>
+			</div>
+		</div>
+	)
+}
+
+export default CustomError
