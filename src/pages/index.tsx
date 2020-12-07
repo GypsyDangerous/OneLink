7bb6681dad8b026ebe4924f51bbe9aa9502b9928
David Snyder <davidgraygs4@gmail.com>
2020-12-06 19:44:26 -0500

    improve style and loading spinner on main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,19 +4,22 @@ import Router from "next/router";
 import { useMutation } from "@apollo/client";
 import loginMutation from "../graphql/loginMutation";
 import useUser from "../hooks/useUser";
+import styled from "styled-components";
+import Loading from "../components/shared/Loading";
+
+const MainPage = styled.div`
+	min-height: 100vh;
+	background: var(--clr-primary-300);
+	color: var(--clr-neutral-100);
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
 
 export default function Home() {
 	// const [loading, setLoading] = useState<boolean>(true)
 
 	const { loading } = useUser({ redirectTo: "/landing", as: "/", loggedInRedirect: "/admin" });
 
-	if (loading) {
-		return <h1>loading...</h1>;
-	}
-
-	return (
-		<div>
-			<h1>Hello next</h1>
-		</div>
-	);
+	return <MainPage>{loading ? <Loading loading={loading} /> : <h1>Hello next</h1>}</MainPage>;
 }
