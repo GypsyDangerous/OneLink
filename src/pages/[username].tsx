cb52d661e3b735a419b4b4e43d5974d6309db81e
David Snyder <davidgraygs4@gmail.com>
2020-12-08 18:32:22 -0500

    fix typo in avatar link

--- src/pages/[username].tsx
+++ src/pages/[username].tsx
@@ -67,7 +67,7 @@ export default function Page(props) {
 			</Head>
 			<Avatar
 				// alt={props.ownerData.username}
-				src={`${process.env.NEXT_PUBLIC_API_URL}/public/imaes/${props.ownerData.photo}`}
+				src={`${process.env.NEXT_PUBLIC_API_URL}/public/images/${props.ownerData.photo}`}
 				className={classes.large}
 			/>
 			<Name>@{props.ownerData.username}</Name>
