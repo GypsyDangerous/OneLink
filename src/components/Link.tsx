6029d681ecdc294aeae812ec5e3d69c058eba9ec
David Snyder <davidgraygs4@gmail.com>
2020-12-08 07:05:04 -0500

    add link component and start rendering links

---
+++ src/components/Link.tsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+const LinkComponent = styled.li`
+	a {
+		padding: 1rem 6rem !important;
+		display: inline-block;
+	}
+	text-align: center;
+	margin: 2rem !important;
+	border: 2px solid white;
+	display: block;
+	&:first-child {
+		margin-top: 0 !important;
+	}
+`;
+
+const Link = ({ name, path }) => {
+	return (
+		<LinkComponent>
+			<a href={path}>{name}</a>
+		</LinkComponent>
+	);
+};
+
+export default Link;
--- src/pages/[username].tsx
+++ src/pages/[username].tsx
@@ -1,47 +1,84 @@
 import client from "../graphql/client";
 import { gql } from "@apollo/client";
 import styled from "styled-components";
+import { publicUserQuery } from "../graphql/userQuery";
+import pageQuery from "../graphql/pageQuery";
+import Link from "../components/Link";
+import { useContext, useEffect, useMemo, useState } from "react";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import Avatar from "@material-ui/core/Avatar";
+import Head from "next/head";
+
+const useStyles = makeStyles((theme: Theme) =>
+	createStyles({
+		large: {
+			width: "100px",
+			height: "100px",
+			marginBottom: "2rem",
+		},
+	})
+);
 
 const UserPage = styled.div`
 	min-height: 100vh;
 	background: var(--clr-primary-300);
 	color: var(--clr-neutral-100);
 	display: flex;
-	justify-content: center;
+	/* justify-content: center; */
+	flex-direction: column;
 	align-items: center;
+	padding-top: 6rem;
+	img {
+		width: 100px;
+		height: 100px;
+	}
 `;
 
-export default function Page(props) {
-	console.log(props)
-	return <UserPage></UserPage>;
+const LinkSet = styled.ul`
+	margin-top: 2rem !important;
+`;
+
+const Name = styled.div`
+	font-weight: bold;
+`;
+
+interface link {
+	name: string;
+	path: string;
+	order: number;
+	color: string;
+	active: boolean;
+	id: string;
 }
 
-const pageQuery = gql`
-	query getPage($name: String!) {
-		page(name: $name) {
-			links {
-				path
-				image
-				name
-				order
-				color
-				active
-				image
-			}
-			theme
-		}
-	}
-`;
+export default function Page(props) {
+	const [links, setLinks] = useState(props.links);
 
-const userQuery = gql`
-	query getUser($name: String!) {
-		user(name: $name) {
-			bio
-			username
-			photo
-		}
-	}
-`;
+	const displayLinks = useMemo(() => {
+		return [...links].sort((a, b) => a.order - b.order);
+	}, [links]);
+
+	const classes = useStyles();
+
+	return (
+		<UserPage>
+			<Head>
+				<title>@{props.ownerData.username} | Onelink</title>
+			</Head>
+			<Avatar
+				// alt={props.ownerData.username}
+				src={`${process.env.NEXT_PUBLIC_API_URL}/public/imaes/${props.ownerData.photo}`}
+				className={classes.large}
+			/>
+			<Name>@{props.ownerData.username}</Name>
+			<LinkSet>
+				{displayLinks.map((link: link) => (
+					<Link key={link.id} {...link} />
+				))}
+			</LinkSet>
+		</UserPage>
+	);
+}
 
 export async function getServerSideProps(ctx) {
 	const { username } = ctx.query;
@@ -50,7 +87,7 @@ export async function getServerSideProps(ctx) {
 		if (!response.data.page) throw new Error("invalid name");
 		const { data } = response;
 		const userResponse = await client.query({
-			query: userQuery,
+			query: publicUserQuery,
 			variables: { name: username },
 		});
 		const { data: userData } = userResponse;
