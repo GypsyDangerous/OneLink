fd9e5a2b18f9024e6fd0ca9cb86bb501401507f1
David Snyder <davidgraygs4@gmail.com>
2020-12-09 17:28:50 -0500

    fix font link

--- src/components/shared/SEO.tsx
+++ src/components/shared/SEO.tsx
@@ -9,7 +9,7 @@ const SEO = ({ title }) => {
 			<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh//GypsyDangerous/simple-css-reset/reset.css"/>
 			<link rel="preconnect" href="https://fonts.gstatic.com" />
 			<link
-				href="https://fonts.googleapis.com/css2?family=Poppins:wght@300400500600700800900&display=swap"
+				href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap"
 				rel="stylesheet"
 			/>
 		</Head>
