7c535197ee2963ae4364429f3343103ca2f90c9a
David Snyder <davidgraygs4@gmail.com>
2020-12-15 18:02:19 -0500

    added split by caps function

--- src/util/functions/index.ts
+++ src/util/functions/index.ts
@@ -2,13 +2,17 @@ export const silentRefresh = async () => {
 	try {
 		const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/refresh_token`, {
 			method: "POST",
-			credentials: "include"
-		})
-		if (!response.ok) return null
-		const json = await response.json()
-		return json?.data?.token
+			credentials: "include",
+		});
+		if (!response.ok) return null;
+		const json = await response.json();
+		return json?.data?.token;
 	} catch (err) {
-		console.log(err)
-		return null
+		console.log(err);
+		return null;
 	}
-}
+};
+
+export const splitByCaps = (string: string) => {
+	return string.split(/(?=[A-Z])/).join(" ");
+};
