22a651b14bcda9b849e211672fca965414730713
David Snyder <davidgraygs4@gmail.com>
2020-12-17 16:22:24 -0500

    move colors to utils folder

--- src/pages/admin/[[...type]].tsx
+++ src/pages/admin/[[...type]].tsx
@@ -14,7 +14,6 @@ import defaultAnimations from "../../util/LinkAnimations.json";
 import defaultTypes from "../../util/LinkTypes.json";
 import { splitByCaps } from "../../util/functions";
 import { settingsContext, SettingsContextProvider } from "../../contexts/settingsContext";
-import chroma from "chroma-js";
 import {
 	AdminPage,
 	AdminSection,
@@ -30,31 +29,7 @@ import {
 } from "../../components/admin/index.styled";
 import { Underline, LargeAvatar } from "../../components/shared/styles";
 import Content from "../../components/admin/Content";
-
-const colors = [
-	"#001aff",
-	"#7000ff",
-	"#ab68ff",
-	"#bd00ff",
-	"#ff6a85",
-	"#282828",
-	"#01a5c8",
-	"#00ffd1",
-	"#00ff19",
-	"#7eff8b",
-	"#ff6767",
-	"#ff7a00",
-	"#e40025",
-	"#e8833e",
-	"#306844",
-	"#007bff",
-	"#fdd618",
-	"#6ab04c",
-	"#e45f00",
-	"#2c4d5a",
-	"#09ad1e",
-	"#ffbcb9",
-].sort((a, b) => chroma(a).luminance() - chroma(b).luminance());
+import { colors } from "../../util/constants";
 
 const Customize = props => {
 	const { settings, update } = useContext(settingsContext);
---
+++ src/util/constants.ts
@@ -0,0 +1,26 @@
+import chroma from "chroma-js"
+
+export const colors = [
+	"#001aff",
+	"#7000ff",
+	"#ab68ff",
+	"#bd00ff",
+	"#ff6a85",
+	"#282828",
+	"#01a5c8",
+	"#00ffd1",
+	"#00ff19",
+	"#7eff8b",
+	"#ff6767",
+	"#ff7a00",
+	"#e40025",
+	"#e8833e",
+	"#306844",
+	"#007bff",
+	"#fdd618",
+	"#6ab04c",
+	"#e45f00",
+	"#2c4d5a",
+	"#09ad1e",
+	"#ffbcb9",
+].sort((a, b) => chroma(a).luminance() - chroma(b).luminance());
