249be5acb6b47527669d86bf3f9df59780c0d27f
David Snyder <davidgraygs4@gmail.com>
2020-12-17 18:35:58 -0500

    add nprogress for route changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -27,6 +27,24 @@ import Loading from "../components/shared/Loading";
 //   console.log(visitorId)
 // })()
 
+import React from "react";
+import Router from "next/router";
+import NProgress from "nprogress";
+
+// NProgress.configure({ showSpinner: publicRuntimeConfig.NProgressShowSpinner });
+
+Router.events.on("routeChangeStart", () => {
+	NProgress.start();
+});
+
+Router.events.on("routeChangeComplete", () => {
+	NProgress.done();
+});
+
+Router.events.on("routeChangeError", () => {
+	NProgress.done();
+});
+
 function App({ children }) {
 	const { loading, ...userData } = useUser({ refresh: true });
 	const router = useRouter();
