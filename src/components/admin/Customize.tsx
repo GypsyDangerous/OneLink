f122c24b9741af3708bdd54ae8839172c9362a7e
David Snyder <davidgraygs4@gmail.com>
2020-12-17 16:24:43 -0500

    move customize section to seperate file

---
+++ src/components/admin/Customize.tsx
@@ -0,0 +1,76 @@
+import { useContext } from "react";
+import LinkComponent from "../../components/Link";
+import { motion } from "framer-motion";
+import { CirclePicker } from "react-color";
+import defaultAnimations from "../../util/LinkAnimations.json";
+import defaultTypes from "../../util/LinkTypes.json";
+import { splitByCaps } from "../../util/functions";
+import { settingsContext } from "../../contexts/settingsContext";
+import {
+	SectionContainer,
+	ContentHeader,
+	ContentSection,
+	CustomizeLinksBody,
+} from "../../components/admin/index.styled";
+import { colors } from "../../util/constants";
+
+const Customize = props => {
+	const { settings, update } = useContext(settingsContext);
+
+	return (
+		<SectionContainer {...props}>
+			<ContentHeader>Background Color</ContentHeader>
+			<ContentSection solid>
+				<CirclePicker
+					colors={colors}
+					width="100%"
+					color={settings.backgroundColor}
+					onChange={color => update("backgroundColor", color.hex)}
+				/>
+			</ContentSection>
+			<ContentHeader>Link Color</ContentHeader>
+			<ContentSection solid>
+				<CirclePicker
+					colors={colors}
+					width="100%"
+					color={settings.linkColor}
+					onChange={color => update("linkColor", color.hex)}
+				/>
+			</ContentSection>
+			<ContentHeader>Link Hover animation</ContentHeader>
+			<ContentSection solid>
+				<CustomizeLinksBody>
+					{defaultAnimations.map(animation => (
+						<motion.div onClick={() => update("animation", animation)}>
+							<h2>{splitByCaps(animation)}</h2>
+
+							<LinkComponent animation={animation} path="" disabled name="Hover Me" />
+						</motion.div>
+					))}
+				</CustomizeLinksBody>
+			</ContentSection>
+			<ContentHeader>Link Style</ContentHeader>
+			<ContentSection solid>
+				<CustomizeLinksBody className="column">
+					{defaultTypes.map(type => {
+						return (
+							<div onClick={() => update("linkStyle", type)}>
+								<h2>{type}</h2>
+								<LinkComponent
+									capsule={type === "capsule"}
+									path=""
+									disabled
+									name="example"
+								/>
+							</div>
+						);
+					})}
+				</CustomizeLinksBody>
+			</ContentSection>
+			<ContentHeader>Style</ContentHeader>
+			<ContentSection solid></ContentSection>
+		</SectionContainer>
+	);
+};
+
+export default Customize;
--- src/pages/admin/[[...type]].tsx
+++ src/pages/admin/[[...type]].tsx
@@ -6,13 +6,9 @@ import LinkComponent from "../../components/Link";
 import Link from "next/link";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import FileCopyIcon from "@material-ui/icons/FileCopy";
-import { motion, AnimateSharedLayout, AnimatePresence } from "framer-motion";
+import { AnimateSharedLayout, AnimatePresence } from "framer-motion";
 import Head from "next/head";
-import { CirclePicker } from "react-color";
 import LinkList from "../../components/shared/LinkList";
-import defaultAnimations from "../../util/LinkAnimations.json";
-import defaultTypes from "../../util/LinkTypes.json";
-import { splitByCaps } from "../../util/functions";
 import { settingsContext, SettingsContextProvider } from "../../contexts/settingsContext";
 import {
 	AdminPage,
@@ -22,74 +18,11 @@ import {
 	PreviewBody,
 	PreviewSection,
 	ContentBody,
-	ContentHeader,
-	ContentSection,
 	AvatarContainer,
-	CustomizeLinksBody,
 } from "../../components/admin/index.styled";
 import { Underline, LargeAvatar } from "../../components/shared/styles";
 import Content from "../../components/admin/Content";
-import { colors } from "../../util/constants";
-
-const Customize = props => {
-	const { settings, update } = useContext(settingsContext);
-
-	return (
-		<SectionContainer {...props}>
-			<ContentHeader>Background Color</ContentHeader>
-			<ContentSection solid>
-				<CirclePicker
-					colors={colors}
-					width="100%"
-					color={settings.backgroundColor}
-					onChange={color => update("backgroundColor", color.hex)}
-				/>
-			</ContentSection>
-			<ContentHeader>Link Color</ContentHeader>
-			<ContentSection solid>
-				<CirclePicker
-					colors={colors}
-					width="100%"
-					color={settings.linkColor}
-					onChange={color => update("linkColor", color.hex)}
-				/>
-			</ContentSection>
-			<ContentHeader>Link Hover animation</ContentHeader>
-			<ContentSection solid>
-				<CustomizeLinksBody>
-					{defaultAnimations.map(animation => (
-						<motion.div onClick={() => update("animation", animation)}>
-							<h2>{splitByCaps(animation)}</h2>
-
-							<LinkComponent animation={animation} path="" disabled name="Hover Me" />
-						</motion.div>
-					))}
-				</CustomizeLinksBody>
-			</ContentSection>
-			<ContentHeader>Link Style</ContentHeader>
-			<ContentSection solid>
-				<CustomizeLinksBody className="column">
-					{defaultTypes.map(type => {
-						return (
-							<div onClick={() => update("linkStyle", type)}>
-								<h2>{type}</h2>
-								<LinkComponent
-									capsule={type === "capsule"}
-									path=""
-									disabled
-									name="example"
-								/>
-							</div>
-						);
-					})}
-				</CustomizeLinksBody>
-			</ContentSection>
-			<ContentHeader>Style</ContentHeader>
-			<ContentSection solid></ContentSection>
-		</SectionContainer>
-	);
-};
-
+import Customize from "../../components/admin/Customize";
 const Analytics = props => {
 	return <SectionContainer {...props}>analytics</SectionContainer>;
 };
