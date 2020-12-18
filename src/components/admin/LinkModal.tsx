dfe68ac38666682d68ae4612897f8c619c3ef42c
David Snyder <davidgraygs4@gmail.com>
2020-12-18 11:01:32 -0500

    added in basic markdown and styles for modal

--- src/components/admin/LinkModal.tsx
+++ src/components/admin/LinkModal.tsx
@@ -1,24 +1,62 @@
-import { forwardRef } from "react";
+import { Dispatch, forwardRef, SetStateAction, useContext } from "react";
 import styled from "styled-components";
-import { ModalMeta } from "../../util/types/Settings";
+import { settingsContext } from "../../contexts/settingsContext";
+import { Link as LinkType, ModalMeta } from "../../util/types/Settings";
+import Link from "../Link";
 
 const ModalComponent = styled.div`
-	background: white;
+	background: #c7e8f3;
 	padding: 1rem;
 	border-radius: 0.5rem;
+	display: grid;
+	grid-template-columns: repeat(2, 1fr);
+	grid-template-rows: repeat(2, 1fr);
+	gap: 0.5rem;
+	h2 {
+		text-transform: capitalize;
+		font-size: 1.5rem;
+		font-weight: bold;
+	}
+`;
+
+const PreviewText = styled.p`
+	justify-self: center;
+`;
+
+const ModalSection = styled.div`
+	background: white;
+	border-radius: 0.25rem;
+	padding: 1rem;
 `;
 
 interface ModalProps {
 	metaData: ModalMeta;
+	currentLink: LinkType;
+	setCurrentLink: Dispatch<SetStateAction<LinkType>>;
 }
 
-const LinkModal = forwardRef<HTMLDivElement, ModalProps>(({ metaData }, ref) => {
-	return (
-		<ModalComponent ref={ref}>
-			<h2>Transition modal</h2>
-			<p id="transition-modal-description">react-transition-group animates me.</p>
-		</ModalComponent>
-	);
-});
+const LinkModal = forwardRef<HTMLDivElement, ModalProps>(
+	({ metaData, currentLink, setCurrentLink }, ref) => {
+		const { settings, update } = useContext(settingsContext);
+
+		return (
+			<ModalComponent ref={ref}>
+				<h2>
+					Add a{"eaiou".includes(metaData?.name?.[0]) && "n"} {metaData?.name}{" "}
+					{metaData?.showUsername ? "username" : "Link"}
+				</h2>
+				<PreviewText>preview</PreviewText>
+				<div>
+					<ModalSection></ModalSection>
+				</div>
+				<div>
+					<ModalSection>
+						<Link {...currentLink} {...settings}></Link>
+					</ModalSection>
+				</div>
+			</ModalComponent>
+		);
+	}
+);
 
 export default LinkModal;
