df7721e49e6fbc7c690026a46c6bbbd3b8cc7279
David Snyder <davidgraygs4@gmail.com>
2020-12-19 22:09:58 -0500

    add preload to font import

--- src/components/shared/SEO.tsx
+++ src/components/shared/SEO.tsx
@@ -20,6 +20,11 @@ const SEO = ({ title }) => {
 			<link rel="icon" type="image/png" href="/circle-cropped.png" />
 			<link
 				href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap"
+				rel="preload"
+				as="style"
+			/>
+			<link
+				href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap"
 				rel="stylesheet"
 			/>
 			<meta
