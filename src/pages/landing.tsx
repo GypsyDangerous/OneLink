26e7e9f05c886b2d61da0cb418f565b65490c740
David Snyder <davidgraygs4@gmail.com>
2020-12-26 10:13:54 -0500

    start working on the landing page

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,5 +1,20 @@
-import {PaddingPage} from "../components/shared/Page.styled"
+import styled from "styled-components";
+import { PaddingPage } from "../components/shared/Page.styled";
+import React, { useState } from "react";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import { FormControl, IconButton, InputLabel, OutlinedInput, TextField } from "@material-ui/core";
+const Hero = styled.section`
+	/* background: blue; */
+	height: calc(100vh - 80px);
+	width: 100vw;
+`;
 
 export default function Landing() {
-	return <PaddingPage>Landing Page</PaddingPage>;
+	return (
+		<PaddingPage>
+			<Hero>
+				<h1>Onelink</h1>
+			</Hero>
+		</PaddingPage>
+	);
 }
