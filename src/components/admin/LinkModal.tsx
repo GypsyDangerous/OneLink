7a9e698102693cf56aeeca43ce03bd31212ff118
David Snyder <davidgraygs4@gmail.com>
2020-12-31 13:36:17 -0500

    remove some console.logs

--- src/components/admin/LinkModal.tsx
+++ src/components/admin/LinkModal.tsx
@@ -179,7 +179,7 @@ const LinkModal = forwardRef<HTMLDivElement, ModalProps>(
 					reader.onload = () => {
 						// Do whatever you want with the file contents
 						const binaryStr = reader.result;
-						console.log(binaryStr);
+						// console.log(binaryStr);
 					};
 					reader.readAsArrayBuffer(file);
 				});
