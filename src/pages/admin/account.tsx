4108f6298256472502dc0b5bb1ec59a1316f33cd
David Snyder <davidgraygs4@gmail.com>
2021-01-10 16:53:46 -0500

    begin adding functionality to account form

--- src/pages/admin/account.tsx
+++ src/pages/admin/account.tsx
@@ -1,5 +1,10 @@
 import styled from "styled-components";
+import Form from "../../components/shared/Form";
 import { PaddingPage } from "../../components/shared/Page.styled";
+import TextField from "@material-ui/core/TextField";
+import useUserContext from "../../hooks/useUserContext";
+import { useEffect, useRef, useState } from "react";
+import { isEqual } from "lodash";
 
 const AccountPage = styled(PaddingPage)`
 	display: flex;
@@ -18,29 +23,87 @@ const AccountSection = styled.div`
 `;
 
 const AccountSectionContent = styled.div`
-	background: white;
+	background: #212121aa;
 	padding: 1rem;
 	width: 100%;
 	margin-top: 1rem;
-	border-radius: .125rem;
+	border-radius: 0.25rem;
+`;
+
+const Actionbutton = styled.button`
+	padding: 0.5rem 1rem;
+	background: var(--clr-accent-300);
+	font-weight: bold;
+	/* color: white; */
+	border-radius: 0.5rem;
+	border: none;
+	outline: none;
+`;
+
+const DangerButton = styled(Actionbutton)`
+	background: #b33a3a;
+	color: white;
 `;
 
 const account = () => {
+	const { user, loading } = useUserContext();
+	const [userData, setUserData] = useState(user);
+
+	const initialUserData = useRef(user);
+
+	useEffect(() => {
+		setUserData({
+			email: user.email,
+			username: user.username,
+		});
+		initialUserData.current = {
+			email: user.email,
+			username: user.username,
+		};
+	}, [loading, user]);
+
+	const userDataModified = !isEqual(initialUserData.current, userData);
+
 	return (
 		<AccountPage>
-			<AccountTitle>My Account</AccountTitle>
-			<AccountSection>
-				<h2>My information</h2>
-				<AccountSectionContent></AccountSectionContent>
-			</AccountSection>
-			<AccountSection>
-				<h2>Account actions</h2>
-				<AccountSectionContent></AccountSectionContent>
-			</AccountSection>
-			<AccountSection>
-				<h2>Danger Zone</h2>
-				<AccountSectionContent></AccountSectionContent>
-			</AccountSection>
+			{user && !loading && (
+				<>
+					<AccountTitle>My Account</AccountTitle>
+					<AccountSection>
+						<h2>My information</h2>
+						<AccountSectionContent>
+							{userData.username && userData.email && (
+								<Form>
+									<TextField
+										label="Name"
+										value={userData.username}
+										onChange={e =>
+											setUserData(prev => ({
+												...prev,
+												username: e.target.value,
+											}))
+										}
+									/>
+									<TextField label="Email" value={userData.email} />
+								</Form>
+							)}
+							{userDataModified && <button>Save</button>}
+						</AccountSectionContent>
+					</AccountSection>
+					<AccountSection>
+						<h2>Account actions</h2>
+						<AccountSectionContent>
+							<Actionbutton>Change Password</Actionbutton>
+						</AccountSectionContent>
+					</AccountSection>
+					<AccountSection>
+						<h2>Danger Zone</h2>
+						<AccountSectionContent>
+							<DangerButton>Delete Account</DangerButton>
+						</AccountSectionContent>
+					</AccountSection>
+				</>
+			)}
 		</AccountPage>
 	);
 };
